161a09c94a1f6be0442264110d1d997a23b2af64
Mariia Katsala <ubuntovka@gmail.com>
2024-07-02 00:00:31 +0200

    Shows real and scheduled time

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 function presenceOfRealDate (item){
     if (item["realDateTime"]){
         return `<tr><td>${item["servingLine"]["number"]}</td><td>${item["servingLine"]["direction"]}
-</td><td>${item["realDateTime"]["hour"]}</td><td>${item["realDateTime"]["minute"]}</td></tr>`;
+</td><td>${item["realDateTime"]["hour"]}:${item["realDateTime"]["minute"]}</td><td>${item["dateTime"]["hour"]}:${item["dateTime"]["minute"]}</td></tr>`;
     } else {
         return `<tr><td>${item["servingLine"]["number"]}</td><td>${item["servingLine"]["direction"]}
-</td><td>${item["dateTime"]["hour"]}</td><td>${item["dateTime"]["minute"]}</td></tr>`;
+</td><td>-</td><td>${item["dateTime"]["hour"]}:${item["dateTime"]["minute"]}</td></tr>`;
     }
 }
 
@@ -15,9 +15,12 @@ async function generateTable() {
     try {
         const response = await fetch('http://localhost:3000/vms');
         const data = await response.json();
-        let table = '<table><tr><th>Number</th><th>Direction</th><th>Hour</th><th>Minute</th></tr>';
-        let table2 = "";
-        table2 += table;
+        let table = '<table>';
+        let table2 = '<table>';
+        table += '<caption><h3>Bus</h3></caption>';
+        table2 += '<caption><h3>Tram</h3></caption>';
+        table += '<tr><th>Number</th><th>Direction</th><th>Real Time</th><th>Scheduled Departure</th></tr>';
+        table2 += '<tr><th>Number</th><th>Direction</th><th>Real Time</th><th>Scheduled Departure</th></tr>';
         data["departureList"].forEach(item => {
             if (item["servingLine"]["name"] === "Bus"){
                 table += presenceOfRealDate(item);
@@ -38,36 +41,3 @@ async function generateTable() {
 }
 
 generateTable();
-
-
-/*var apiUrl = "http://localhost:3000/vms";
-const number = document.getElementById('number');
-const number2 = document.getElementById('number2');
-const direction = document.getElementById('direction');
-const direction2 = document.getElementById('direction2');
-const hour = document.getElementById('hour');
-const hour2 = document.getElementById('hour2');
-const minute = document.getElementById('minute');
-const minute2 = document.getElementById('minute2');
-
-fetch(apiUrl)
-  .then((res) => {
-    return res.json();
-  }).then((data) => {
-  for (let i = 0; data["departureList"].length; i++) {
-    if (data["departureList"][0]["servingLine"]["name"] === "Bus"){
-      number.textContent = JSON.stringify(data["departureList"][i]["servingLine"]["number"]);
-      direction.textContent = JSON.stringify(data["departureList"][i]["servingLine"]["direction"]);
-      hour.textContent += JSON.stringify(data["departureList"][i]["realDateTime"]["hour"]);
-      minute.textContent += JSON.stringify(data["departureList"][i]["realDateTime"]["minute"]);
-    } else {
-      number2.textContent = JSON.stringify(data["departureList"][i]["servingLine"]["number"]);
-      direction2.textContent = JSON.stringify(data["departureList"][i]["servingLine"]["direction"]);
-      hour2.textContent += JSON.stringify(data["departureList"][i]["realDateTime"]["hour"]);
-      minute2.textContent += JSON.stringify(data["departureList"][i]["realDateTime"]["minute"]);
-    }
-  }
-
-
-  })
-  .catch((err) => console.debug(err)); */
