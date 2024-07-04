86e3dbd4d1d6abcc9c3b4a228d1530dc82cb0002
Mariia Katsala <ubuntovka@gmail.com>
2024-07-04 17:37:17 +0200

    Shows a navigation bar with available transport

--- js/main.js
+++ js/main.js
@@ -1,6 +1,61 @@
 
 //var apiUrl = "https://efa.vvo-online.de/VMSSL3/XSLT_DM_REQUEST?language=de&mode=direct&name_dm=Chemnitz%2C+Robert-Siewert-Str&nameInfo_dm=36030050&type_dm=any&useRealtime=1&outputFormat=JSON";
 
+function showAlert() {
+    alert("Hi Masha!");
+}
+
+// function showTransport(transportType) {
+//     generateTableForTransport(transportType);
+// }
+
+async function liGenerator() {
+    try {
+        const response = await fetch('http://localhost:3000/vms');
+        const data = await response.json();
+        let navBar = '<ul>';
+        const transportsSet = new Set();
+        data["departureList"].forEach(item => {
+            if(!transportsSet.has(item["servingLine"]["name"])){
+                navBar += `<li><button onclick="showTransport('${item["servingLine"]["name"]}')">${item["servingLine"]["name"]}</button></li>`;
+            }
+            transportsSet.add(item["servingLine"]["name"]);
+        });
+        navBar += '</ul>';
+        const tableContainer = document.getElementById('ulli_container');
+        tableContainer.innerHTML = navBar;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+liGenerator();
+
+
+async function showTransport(transportType) {
+    try {
+        const response = await fetch('http://localhost:3000/vms');
+        //const response = await fetch("https://efa.vvo-online.de/VMSSL3/XSLT_DM_REQUEST?language=de&mode=direct&name_dm=Chemnitz%2C+Robert-Siewert-Str&nameInfo_dm=36030050&type_dm=any&useRealtime=1&outputFormat=JSON");
+        const data = await response.json();
+        let table = '<table>';
+        table += `<caption><h3>${transportType}</h3></caption>`;
+        table += '<tr><th>Number</th><th>Direction</th><th>Real Time</th><th>Scheduled Departure</th></tr>';
+        data["departureList"].forEach(item => {
+            if (item["servingLine"]["name"] === transportType) {
+                table += presenceOfRealDate(item);
+            }
+        });
+
+        table += '</table>';
+
+        const tableContainer = document.getElementById('table_container');
+        tableContainer.innerHTML = table;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 function presenceOfRealDate (item){
     if (item["realDateTime"]){
         return `<tr><td>${item["servingLine"]["number"]}</td><td>${item["servingLine"]["direction"]}
@@ -13,8 +68,8 @@ function presenceOfRealDate (item){
 
 async function generateTable() {
     try {
-        //const response = await fetch('http://localhost:3000/vms');
-        const response = await fetch("https://efa.vvo-online.de/VMSSL3/XSLT_DM_REQUEST?language=de&mode=direct&name_dm=Chemnitz%2C+Robert-Siewert-Str&nameInfo_dm=36030050&type_dm=any&useRealtime=1&outputFormat=JSON");
+        const response = await fetch('http://localhost:3000/vms');
+        //const response = await fetch("https://efa.vvo-online.de/VMSSL3/XSLT_DM_REQUEST?language=de&mode=direct&name_dm=Chemnitz%2C+Robert-Siewert-Str&nameInfo_dm=36030050&type_dm=any&useRealtime=1&outputFormat=JSON");
         const data = await response.json();
         let table = '<table>';
         let table2 = '<table>';
@@ -41,4 +96,5 @@ async function generateTable() {
     }
 }
 
-generateTable();
+//generateTable();
+
